be22c3c33a4bdb5cc65e209bdaa74e646276e4fd
Łukasz Dudek <lukdud@gmail.com>
2023-04-02 23:22:58 +0200

    Add project files.

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    class Program
+    { 
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Let's check the number you choose is even or odd!");
+            Console.WriteLine("Choose your number, please.");
+            
+            string number = Console.ReadLine();
+            
+            int chosenNumber;
+            
+            Int32.TryParse(number, out chosenNumber);
+
+            if(chosenNumber %2 == 0) 
+            {
+                Console.WriteLine($"Your number \"{number}\" is even");
+            }
+            else
+            {
+                Console.WriteLine($"Your number \"{number}\" is odd");
+            }
+        }
+    }
+}
